8caebe9592c7dd9b688aaf86437097dd485c24c6
Michael Joseph <michaeljoseph+github@gmail.com>
2016-06-09 21:45:57 +0200

    Fix context references and use jinja-time support

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'{{cookiecutter.project_name}}'
-copyright = u'{{cookiecutter.year}}, {{cookiecutter.full_name}}'
+copyright = u'{% now 'utc', '%Y' %}, {{cookiecutter.name}}'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -201,7 +201,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', '{{cookiecutter.repo_name}}.tex', u'{{cookiecutter.project_name}} Documentation',
-   u'{{cookiecutter.full_name }}', 'manual'),
+   u'{{cookiecutter.name }}', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -231,7 +231,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', '{{cookiecutter.repo_name}}', u'{{cookiecutter.project_name}} Documentation',
-     [u'{{cookiecutter.full_name}}'], 1)
+     [u'{{cookiecutter.name}}'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -245,7 +245,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', '{{cookiecutter.repo_name}}', u'{{cookiecutter.project_name}} Documentation',
-   u'{{cookiecutter.full_name}}', '{{cookiecutter.repo_name}}', 'One line description of project.',
+   u'{{cookiecutter.name}}', '{{cookiecutter.repo_name}}', 'One line description of project.',
    'Miscellaneous'),
 ]
 
